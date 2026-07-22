4cee4b6c3811926a7a60a6c5812c2acd1047e6bb
Jammy555 <prathap.venkata@gmail.com>
2026-07-22 06:38:49 +0000

    sm8350-common: Add audio patch to vendorsetup.sh
    Signed-off-by: Jammy555 <prathap.venkata@gmail.com>
    Change-Id: I42b9274666e7174acfc3dba1ddfd546668657fdb

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -14,7 +14,14 @@ apply_patch() {
         elif git apply "$patch_file" >/dev/null 2>&1; then
             echo "${target_dir}: Successfully applied ($name)"
         else
-            echo "${target_dir}: Failed to apply ($name)"
+            echo "========================================================================"
+            echo " [!] MERGE CONFLICT / PATCH ERROR"
+            echo " Target: ${target_dir}"
+            echo " Patch Name: ${name}"
+            echo " Patch File: ${patch_file}"
+            echo " Details of failure:"
+            git apply --check "$patch_file"
+            echo "========================================================================"
         fi
         popd >/dev/null
     fi
@@ -23,3 +30,4 @@ apply_patch() {
 apply_patch "packages/apps/GameSpace" "../../../$PATCH_DIR/gamespace_sync.patch" "GameSpace Sync"
 apply_patch "frameworks/base" "../../$PATCH_DIR/frameworks_base_a0fed77.patch" "Oplus Framework Stubs"
 apply_patch "device/qcom/sepolicy_vndr/legacy-um" "../../../../$PATCH_DIR/sepolicy_vndr_vendor_modprobe.patch" "Vendor Modprobe Sepolicy"
+apply_patch "hardware/qcom-caf/sm8350/audio" "../../../../$PATCH_DIR/hardware_qcom_audio_lahaina_drop_hw_acc.patch" "Audio: Drop hw_acc effect"
