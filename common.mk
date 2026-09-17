d99058c979339acde27d56a8f43f3eded72021cc
Jammy555 <prathap.venkata@gmail.com>
2026-09-17 16:48:43 +0530

    sm8350-common: Explicitly disable LiveDisplay PictureAdjustment
    causing LiveDisplayService to throw UnsupportedOpereation error
    
    Change-Id: I817eb471cb94e8a86182792c1c440a9050cffc8f

--- common.mk
+++ common.mk
@@ -209,6 +209,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.livedisplay-service.oplus
 
+$(call soong_config_set_bool,OPLUS_LINEAGE_LIVEDISPLAY_HAL,ENABLE_PA,false)
+
 # Media
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/media/media_codecs_lahaina_vendor.xml:$(TARGET_COPY_OUT_VENDOR)/etc/media_codecs_lahaina.xml \
